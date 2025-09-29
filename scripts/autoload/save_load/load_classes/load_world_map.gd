027cae8532e4001166a8f6b4e40277886294040b
Zénó Kozma <zeno.kozma03@gmail.com>
2025-09-29 21:04:02 +0100

    chore: final cleanup on branch

--- scripts/autoload/save_load/load_classes/load_world_map.gd
+++ scripts/autoload/save_load/load_classes/load_world_map.gd
@@ -9,7 +9,6 @@ static func load_biome_type_data() -> void:
 		WorldMapData.biome_type = file.get_var()
 		file.close()
 
-
 static func load_world_map_savagery() -> void:
 	if FileAccess.file_exists(SavePaths.world_map_savagery_save):
 		var file = FileAccess.open_compressed(SavePaths.world_map_savagery_save, FileAccess.READ)
@@ -26,7 +25,6 @@ static func load_world_maps() -> void:
 	WorldMapData.world_map2.load_world_map()
 	WorldMapData.biome_map.load_biome_map()
 	WorldMapData.world_monster_map.load_world_monster_map()
-
 
 # BASE SAVES
 
